67d9f7482a5f18d835cd24c7e6caf61b70159bea
khuatdinhsang <sangkdhe161711@fpt.edu.vn>
2024-03-21 14:44:14 +0700

    fix api chat

--- src/app/app.gateway.ts
+++ src/app/app.gateway.ts
@@ -1,6 +1,6 @@
-import { AuthService } from './../modules/auth/auth.service';
 /* eslint-disable prettier/prettier */
 import { MessageService } from './../modules/message/message.service';
+import { AuthService } from './../modules/auth/auth.service';
 import {
   SubscribeMessage,
   WebSocketGateway,
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { Message } from 'src/modules/message/schemas/message.schemas';
+import { RoomService } from 'src/modules/room/room.service';
 @WebSocketGateway({
   cors: {
     origin: '*',
@@ -18,14 +19,33 @@ import { Message } from 'src/modules/message/schemas/message.schemas';
 })
 export class AppGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  constructor(private messageService: MessageService, private authService: AuthService) { }
+  constructor(private messageService: MessageService,
+    private authService: AuthService,
+    private roomService: RoomService
+
+  ) { }
 
   @WebSocketServer() server: Server;
 
   @SubscribeMessage('sendMessage')
   async handleSendMessage(client: Socket, payload: Message): Promise<void> {
-    await this.messageService.createMessage(payload);
-    this.server.emit('recMessage', payload);
+    console.log("client", client)
+    console.log("clientId", client.id)
+    const [result] = await Promise.all([
+      this.messageService.createMessage(payload),
+      this.roomService.updateChatRoomService({
+        sender_id: payload.sender_id,
+        receiver_id: payload.receiver_id,
+        lastMessage: payload.content
+      })
+    ]);
+    console.log("result", result)
+    if (client.id) {
+      client.to(client.id).emit('receive_message', {
+        payload: result
+      });
+    }
+    // this.server.emit('recMessage', payload);
   }
 
   afterInit(server: Server) {
@@ -44,7 +64,6 @@ export class AppGateway
     if (authHeader && (authHeader as string).split('')[1]) {
       try {
         socket.data = await this.authService.handleVerifyToken((authHeader as string).split('')[1])
-        console.log("sd", socket.data)
       } catch (error) {
 
       }
--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -25,23 +25,58 @@ export class RoomService {
                     },
                 },
                 updatedAt: 1,
-                last_message: 1,
+                lastMessage: 1,
             },
         )
             .sort({ updatedAt: -1 })
-            .populate('users', '_id fullName avatar');
+            .populate('users', '_id fullName avatar role');
         return chatRoomList;
     }
     async getOneChatRoom(chat_room_id: string) {
         const chatRoomDetail = await this.roomModel.findOne({
             _id: chat_room_id,
-        }).populate('users', '_id fullName avatar');
+        }).populate('users', '_id fullName avatar role');
         return chatRoomDetail;
     }
     async createChatRoom(payload: ChatRoomDto) {
-        const chatRoom = await this.roomModel.create({
-            ...payload,
-        });
-        return chatRoom;
+        const allRoom = await this.roomModel.find()
+        let check = false;
+        for (const room of allRoom) {
+            let foundUsers = 0;
+            for (const user of payload.users) {
+                if (room.users.includes(user)) {
+                    foundUsers++;
+                }
+            }
+            if (foundUsers === payload.users.length) {
+                check = true
+                // Cả hai người dùng đều tồn tại trong ít nhất một phòng
+            }
+        }
+        if (!check) {
+            const chatRoom = await this.roomModel.create({
+                ...payload,
+            });
+            return chatRoom;
+        }
+        const room = await this.roomModel.findOne({ users: payload.users })
+        return room
     }
+    async updateChatRoomService({
+        sender_id,
+        receiver_id,
+        lastMessage,
+    }) {
+        try {
+            await this.roomModel.findOneAndUpdate(
+                {
+                    users: { $all: [sender_id, receiver_id] },
+                },
+                { lastMessage },
+            );
+        } catch (error) {
+            console.log('error', error);
+        }
+    }
+
 }
